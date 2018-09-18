6bd74590b1c18edda5684cd176e1894729f395a7
Mangesh <mangesh.maurya24@gmail.com>
2018-09-18 11:20:23 +0530

    new file:   .classpath 	new file:   .project 	new file:   .settings/org.eclipse.jdt.core.prefs 	new file:   bin/com/testng/ex/FirstScript.class 	new file:   bin/com/testng/ex/test_sos.class 	new file:   src/com/testng/ex/FirstScript.java 	new file:   src/com/testng/ex/test_sos.java 	new file:   test-output/Default suite/Default test.html 	new file:   test-output/Default suite/Default test.xml 	new file:   test-output/bullet_point.png 	new file:   test-output/collapseall.gif 	new file:   test-output/emailable-report.html new file:   test-output/failed.png 	new file:   test-output/index.html 	new file:   test-output/jquery-1.7.1.min.js 	new file:   test-output/junitreports/TEST-com.testng.ex.FirstScript.xml 	new file:   test-output/navigator-bullet.png 	new file:   test-output/old/Default suite/Default test.properties 	new file:   test-output/old/Default suite/classes.html 	new file:   test-output/old/Default suite/groups.html 	new file:   test-output/old/Default suite/index.html 	new file:   test-output/old/Default suite/main.html 	new file:   test-output/old/Default suite/methods-alphabetical.html 	new file:   test-output/old/Default suite/methods-not-run.html 	new file:   test-output/old/Default suite/methods.html 	new file:   test-output/old/Default suite/reporter-output.html 	new file:   test-output/old/Default suite/testng.xml.html 	new file:   test-output/old/Default suite/toc.html 	new file:   test-output/old/index.html 	new file:   test-output/passed.png 	new file:   test-output/skipped.png 	new file:   test-output/testng-reports.css 	new file:   test-output/testng-reports.js 	new file:   test-output/testng-results.xml 	new file:   test-output/testng.css

---
+++ src/com/testng/ex/FirstScript.java
@@ -0,0 +1,10 @@
+package com.testng.ex;
+
+import org.testng.annotations.Test;
+
+public class FirstScript {
+  @Test
+  public void test1() {
+	  System.out.println("Hello world");
+  }
+}
---
+++ src/com/testng/ex/test_sos.java
@@ -0,0 +1,12 @@
+package com.testng.ex;
+
+public class test_sos {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hELLO");
+		
+
+	}
+
+}
